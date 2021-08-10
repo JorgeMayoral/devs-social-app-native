ddadb6d7819d14fc086221b3c4aa7bf5705d79e6
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-10 17:58:10 +0200

    feat: :sparkles: Add register service

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Constants from 'expo-constants'
-import { SignInData } from '../types'
+import { SignInData, SignUpData } from '../types'
 import { saveToken } from '../utils/tokenStorage'
 
 const extra = Constants.manifest?.extra
@@ -11,6 +11,19 @@ export const login = async (data: SignInData): Promise<boolean> => {
   const response = await axios.post(url, data)
 
   if (response.status === 200) {
+    await saveToken(response.data.token)
+    return true
+  }
+
+  return false
+}
+
+export const register = async (data: SignUpData): Promise<boolean> => {
+  const url = `${extra?.apiUrl}/v1/user/register`
+
+  const response = await axios.post(url, data)
+
+  if (response.status === 201) {
     await saveToken(response.data.token)
     return true
   }
