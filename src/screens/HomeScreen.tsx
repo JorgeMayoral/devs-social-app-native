c10d186e0f07ca3624d16d2c07b83c997d1b735f
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-11 17:03:35 +0200

    perf: :zap: Update home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import {
   FlatList,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import Navbar from '../components/Navbar';
@@ -31,10 +32,12 @@ const HomeScreen = ({ navigation }: any) => {
 
   const getMorePosts = async () => {
     const data = await fetchTimeline(currentPostsNumber);
-    setPosts(posts.concat(data));
-    setCurrentPostsNumber((prev) => prev + 10);
-    if (data.length === 0) {
+
+    if (data.length === 0 || data[0] === null) {
       setEndReached(true);
+    } else {
+      setPosts(posts.concat(data));
+      setCurrentPostsNumber((prev) => prev + 10);
     }
   };
 
@@ -51,16 +54,29 @@ const HomeScreen = ({ navigation }: any) => {
       {posts.length > 0 ? (
         <FlatList
           data={posts}
-          renderItem={({ item }) => <PostCard postData={item} />}
+          renderItem={({ item }) => (
+            <PostCard
+              postData={item}
+              callback={() =>
+                navigation.navigate('User', { userId: item.authorId })
+              }
+            />
+          )}
           keyExtractor={(item) => item.id}
           onRefresh={getPosts}
           refreshing={loading}
           onEndReached={getMorePosts}
           onEndReachedThreshold={0.25}
           ListFooterComponent={MoreLoading}
+          style={styles.postList}
         />
       ) : (
-        <Text>There are no posts, try following someone.</Text>
+        <View>
+          <Text>There are no posts, try following someone.</Text>
+          <TouchableOpacity onPress={getPosts}>
+            <Text style={styles.link}>Reload</Text>
+          </TouchableOpacity>
+        </View>
       )}
       <Navbar navigator={navigation} currentRoute="Home" />
     </View>
@@ -72,6 +88,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  postList: {
+    flex: 1,
+    width: '100%',
+  },
+  link: {
+    color: 'blue',
   },
 });
 
