6801f34c9b4b6559a334a84bf18867be4c199dc0
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-11 17:02:35 +0200

    perf: :zap: Change user default to undefined

--- src/utils/userStore.ts
+++ src/utils/userStore.ts
@@ -2,7 +2,7 @@ import { fetchProfile } from '../services/userService'
 import create from 'zustand'
 
 export const useUserStore = create<any>(set => ({
-  user: {},
+  user: undefined,
   fetch: async () => {
     const data = await fetchProfile()
     set({user: data})
