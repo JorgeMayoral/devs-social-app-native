c60c4ed321d0efe936eaca4935326124e2dd6622
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-11 18:14:23 +0200

    feat: :sparkles: Add routes to screens

--- App.tsx
+++ App.tsx
@@ -1,8 +1,68 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Main from './src/components/Main';
+import { useToken } from './src/hooks/useToken';
+import ExploreScreen from './src/screens/ExploreScreen';
+// Screens
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import PublishScreen from './src/screens/PublishScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import UserScreen from './src/screens/UserScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <Main />;
+  const { token } = useToken();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={token ? 'Home' : 'SignIn'}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignInScreen}
+          options={{ title: 'Sign in' }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ title: 'Sign up' }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: 'Profile' }}
+        />
+        <Stack.Screen
+          name="Publish"
+          component={PublishScreen}
+          options={{ title: 'Publish' }}
+        />
+        <Stack.Screen
+          name="Explore"
+          component={ExploreScreen}
+          options={{ title: 'Explore' }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: 'Settings' }}
+        />
+        <Stack.Screen
+          name="User"
+          component={UserScreen}
+          options={{ title: 'User' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
